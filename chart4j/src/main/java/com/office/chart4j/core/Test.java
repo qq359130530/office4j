b4e5d66803dad6ab370a8ebe3e8ae0ef0277e627
赵凡 <zhaofan.wh@1218.com.cn>
2021-09-06 16:46:47 +0800

    新增chart4j

--- chart4j/src/main/java/com/office/chart4j/core/Test.java
+++ chart4j/src/main/java/com/office/chart4j/core/Test.java
@@ -6,6 +6,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.StandardChartTheme;
 import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
 import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 
 import java.awt.*;
@@ -30,8 +31,25 @@ public class Test {
         // 应用主题样式
         ChartFactory.setChartTheme(theme);
         OutputStream outputStream = new FileOutputStream(new File("C:\\Users\\Rzxuser\\Desktop\\chart.jpg"));
-        ChartUtils.writeChartAsJPEG(outputStream, createPieChart(), 500, 500);
+        ChartUtils.writeChartAsJPEG(outputStream, createBarChart(), 500, 500);
         outputStream.close();
+    }
+
+    // 条形图
+    public static JFreeChart createBarChart() {
+        DefaultCategoryDataset dateset = new DefaultCategoryDataset();
+        dateset.addValue(1.0, "宝马", "速度");
+        dateset.addValue(2.0, "奥迪", "速度");
+        dateset.addValue(3.0, "奔驰", "速度");
+        dateset.addValue(2.0, "宝马", "价格");
+        dateset.addValue(2.0, "奥迪", "价格");
+        dateset.addValue(1.0, "奔驰", "价格");
+        dateset.addValue(2.0, "宝马", "空间");
+        dateset.addValue(2.0, "奥迪", "空间");
+        dateset.addValue(3.0, "奔驰", "空间");
+
+        JFreeChart chart = ChartFactory.createBarChart("汽车对照表", "参数", "分值", dateset);
+        return chart;
     }
 
     // 饼图
